c39946abcb187e6f112ac941fd394aba9a5a1db4
THIENVA <v.voanhthien@gmail.com>
2026-02-02 11:29:50 +0700

    project-commit

--- src/ProjectInfo.tsx
+++ src/ProjectInfo.tsx
@@ -7,6 +7,7 @@ function PrjInfo({ name, desc }: PrjInfoProps) {
   return (
     <div className="font-bold">
       <h2>{name}</h2>
+      <div className="w-20 h-1 bg-orange-500"></div>
       <h4>{desc}</h4>
     </div>
   );
