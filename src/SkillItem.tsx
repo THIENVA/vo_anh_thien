aad27e59f6ace283f058920509f206b0ea51beca
THIENVA <v.voanhthien@gmail.com>
2026-02-02 10:38:35 +0700

    Skill-commit-9

--- src/SkillItem.tsx
+++ src/SkillItem.tsx
@@ -5,8 +5,15 @@ type SkillItemProps = {
 
 function SkillItem({ icon, title }: SkillItemProps) {
   return (
-    <div className="skill-item flex flex-col items-center ml-[100px]">
-      <img src={icon} alt={title} className="object-contain p-2 rounded-xl" />
+    <div className="skill-item flex flex-col items-center">
+      <img
+        src={icon}
+        alt={title}
+        className="w-16 h-16
+          object-contain
+          p-2
+          rounded-xl"
+      />
       <h3 className="mt-3 text-xl font-bold text-[#000]">{title}</h3>
     </div>
   );
