7e07c309a95b0d1ff4184ca548b6e5fd3065d818
THIENVA <v.voanhthien@gmail.com>
2026-02-25 16:06:27 +0700

    img-update

---
+++ public/img/index.ts
@@ -0,0 +1,49 @@
+import heroIntro31 from "./her0-intro-3.1.svg";
+import heroIntro32 from "./her0-intro-3.2.svg";
+import heroIntro41 from "./her0-intro-4.1.svg";
+import heroIntro42 from "./her0-intro-4.2.svg";
+import heroIntro43 from "./her0-intro-4.3.svg";
+import heroImage from "./hero-image.jpg";
+import aboutImage from "./482252446_4007445956166062_6666068470433458609_1.jpg";
+import html from "./html-icon.svg";
+import css from "./CSS-icon.svg";
+import js from "./JS-icon.svg";
+import react from "./react-icon.svg";
+import ubuntu from "./ubuntu-icon.svg";
+import aws from "./aws-icon.svg";
+import docker from "./docker-icon.svg";
+import pnj from "./logo-pnj-vector-inkythuatso-2-01-17-14-52-42 1.png";
+import ntt from "./Logo-NTT-SV 1.png";
+import vinatech from "./Logo-01 1.png";
+import project1 from "./Screenshot 2025-12-31 213136 1.png";
+import project2 from "./Screenshot 2025-12-31 214315 2.png";
+import phone from "./phone-icon.svg";
+import mail from "./mail-icon.svg";
+import address from "./address-icon.svg";
+import github from "./github-icon.svg";
+
+export {
+  heroIntro31,
+  heroIntro32,
+  heroIntro41,
+  heroIntro42,
+  heroIntro43,
+  heroImage,
+  aboutImage,
+  html,
+  css,
+  js,
+  react,
+  ubuntu,
+  aws,
+  docker,
+  pnj,
+  ntt,
+  vinatech,
+  project1,
+  project2,
+  phone,
+  mail,
+  address,
+  github,
+};
