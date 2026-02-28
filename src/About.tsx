8fecdab4c8a96e59dc3cadd0cae51db62cbef1e7
THIENVA <v.voanhthien@gmail.com>
2026-02-28 14:39:14 +0700

    update-about-data

--- src/About.tsx
+++ src/About.tsx
@@ -1,30 +1,21 @@
-import { aboutImage } from "./assets";
 import Block from "./Block";
 import Button from "./Button";
+import dataAbout from "./data/about.json";
 
 function About() {
   return (
     <Block>
       <Button title="ABOUT ME" onClick={() => console.log("clicked")} />
       <div className="flex gap-12 m-auto">
-        <img src={aboutImage} alt="about-image" className="w-55 h-50" />
+        <img src={dataAbout.image} alt="about-image" className="w-55 h-50" />
         <div className="text-[#d67f1f] text-[110%] leading-relaxed">
-          <p className="mt-5">
-            I'm a passionate, self-proclaimed developer who specializes in full
-            stack development (React.js & Node.js). I am very enthusiastic about
-            bringing the technical and visual aspects of digital products to
-            life. User experience, pixel perfect design, and writing clear,
-            readable, highly performant code matters to me. I am very much a
-            progressive thinker and enjoy working on products end to end, from
-            ideation all the way to development.
-          </p>
+          <p className="mt-5">{dataAbout.description}</p>
           <div className="mt-6">
-            <p>Finally, some opportunities that's my interest.</p>
+            <p>{dataAbout.subtitle} </p>
             <ul className="list-disc list-inside mt-3 space-y-2">
-              <li>Front-end developer</li>
-              <li>IT Technician</li>
-              <li>System Administrator</li>
-              <li>Website Administrator</li>
+              {dataAbout.interest.map((item, index) => (
+                <li key={index}>{item}</li>
+              ))}
             </ul>
           </div>
         </div>
