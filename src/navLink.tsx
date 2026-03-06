012562f38f8835ae72165fb741a46692b59d5300
THIENVA <v.voanhthien@gmail.com>
2026-03-06 13:44:45 +0700

    update-navLink

--- src/navLink.tsx
+++ src/navLink.tsx
@@ -1,16 +1,33 @@
+import { Link } from "react-router-dom";
+
 type NavLinkProps = {
-  icon: string; // đường dẫn image
-  title: string; // nội dung h3
+  icon: string;
+  title: string;
 };
 
 function NavLink({ icon, title }: NavLinkProps) {
+  const isRoute = icon.startsWith("/");
+
+  if (isRoute) {
+    // Dùng React Router Link cho các route như /blog
+    return (
+      <Link
+        to={icon}
+        className="text-[#e7870a] font-bold hover:text-[hsl(242,90%,45%)]"
+      >
+        {title}
+      </Link>
+    );
+  }
+
+  // Dùng <a> bình thường cho anchor #about, #skill...
   return (
-    <a
-      href={icon}
+    <Link
+      to={icon}
       className="text-[#e7870a] font-bold hover:text-[hsl(242,90%,45%)]"
     >
       {title}
-    </a>
+    </Link>
   );
 }
 
